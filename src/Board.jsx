f9af7d1725605b07b737a8ac8955f8e571833cf8
Dinesh Bala <baladnes@gmail.com>
2022-07-19 09:05:53 +0545

    Setup squares index and state

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  console.log(squares);
+
   return (
     <div className="board-container">
       <h2>Tic-Tac-Toe</h2>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </div>
   );
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Square = () => {
-  return <button className="square">O</button>;
+const Square = ({ value }) => {
+  return (
+    <button className="square" >
+      {value}
+    </button>
+  );
 };
 
 export default Square;
