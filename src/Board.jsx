ecf00431d6440db4868c242323b3eceae666868a
Dinesh Bala <baladnes@gmail.com>
2022-07-20 07:44:15 +0545

    Declared winner

--- src/Board.jsx
+++ src/Board.jsx
@@ -8,13 +8,41 @@ const Board = () => {
   console.log(squares);
 
   const handleClick = (i) => {
-    if (squares[i]) {
+    if (checkWinner(squares) || squares[i]) {
       return;
     }
     const newSquares = [...squares];
     newSquares[i] = xTurn ? "X" : "O";
     setSquares(newSquares);
     setXTurn(!xTurn);
+  };
+
+  const checkWinner = (squares) => {
+    const winningLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningLines.length; i++) {
+      // winningLines[0] = [0, 1, 2]
+      const a = winningLines[i][0]; // index stored : 0
+      const b = winningLines[i][1]; // 1
+      const c = winningLines[i][2];
+      console.log(a, b, c);
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   };
 
   return (
